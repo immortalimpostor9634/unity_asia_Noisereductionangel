d90d445e33794ef3053e6150e7925487118e057b
immortalimpostor9634 <a21150288@gmail.com>
2021-12-14 21:39:46 +0800

    檢查地板:繪製圖形

--- unity_asia_Noisereductionangel/Assets/scripts/controller2D.cs
+++ unity_asia_Noisereductionangel/Assets/scripts/controller2D.cs
@@ -7,11 +7,17 @@ public class controller2D : MonoBehaviour
 {
     #region 欄位 : 公開
 
-    [Header("移動速度"), Range(0, 500)]
+    [Header("移動速度"), Range(0, 50)]
     public float speed = 3.5f;
 
     [Header("跳躍高度"), Range(0, 1000)]
     public float jumped = 300;
+
+    [Header("檢查地板尺寸與位移")]
+    [Range(0, 1)]
+    public float checkgroundradius = 0.1f;
+    public Vector3 checkgroundoffest;
+
 
     #endregion
 
@@ -19,6 +25,16 @@ public class controller2D : MonoBehaviour
     /// 剛體元件 Rigidbody 2D
     /// </summary>
     private Rigidbody2D rig;
+
+    /// <summary>
+    /// 繪製圖示
+    /// 在unity中繪製輔助用的圖示
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0.2f, 0.3f);
+        Gizmos.DrawSphere(transform.position, checkgroundradius);
+    }
 
     private void Start()
     {
@@ -33,6 +49,11 @@ public class controller2D : MonoBehaviour
         move();
     }
 
+    private void Update()
+    {
+        flip();
+    }
+
     #region 方法
     /// <summary>
     /// 1.玩家是否有按方向鍵
@@ -42,6 +63,31 @@ public class controller2D : MonoBehaviour
     {
         float hori = Input.GetAxis("Horizontal");
         print(" 玩家左右鍵值:" + hori);
+
+        // 剛體元件.加速度 = 新二維向量( 區域變數名稱*移動速度,0)
+        rig.velocity = new Vector2(hori * speed, rig.velocity.y);
+    }
+
+    /// <summary>
+    /// 翻面
+    /// 當 hori < 0 (角色往左移動) ，角度 Y = 180
+    /// 當 hori > 0 (角色往右移動) ，角度 Y = 0
+    /// </summary>
+    private void flip()
+    {
+        float hori = Input.GetAxis("Horizontal");
+
+        // 當 hori < 0 (角色往左移動) ，角度 Y = 180
+        if ( hori < 0 )
+        {
+            transform.eulerAngles = new Vector3(0,180,0);
+        }
+
+        // 當 hori > 0 (角色往右移動) ，角度 Y = 0
+        if (hori > 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
     }
     #endregion
 }
